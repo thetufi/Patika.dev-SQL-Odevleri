39665d91bb23745a161d84396e5ce423a61f8e3b
tufan erkul <tufan_erkul@hotmail.com>
2022-03-13 12:34:39 +0300

    Create 4.1.sql
    count ve distinct ödevi

---
+++ 4.1.sql
@@ -0,0 +1,11 @@
+# count ve distinct ödevi
+
+- select distinct replacement_cost FROM film
+
+- select count(distinct replacement_cost) FROM film
+
+- select count(title) FROM film where title like 'T%' AND rating = 'G'
+
+- select count'*' from country where country like '_____'
+
+- select count(*) from city where city ilike '%r'
